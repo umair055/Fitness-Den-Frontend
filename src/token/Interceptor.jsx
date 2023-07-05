c8a5414821b6e13b854e1fc0cd0a87eae3148410
umair055 <umairtariq403@gmail.com>
2023-07-05 17:26:14 +0500

    finalized

--- src/token/Interceptor.jsx
+++ src/token/Interceptor.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-axios.defaults.baseURL =
-  "https://tasty-slug-handkerchief.cyclic.app/fitness-den/";
+axios.defaults.baseURL = "https://fitnessden.herokuapp.com/fitness-den/";
+// axios.defaults.baseURL = "http://localhost:5000/fitness-den/";
 let refresh = false;
 
 axios.interceptors.response.use(
